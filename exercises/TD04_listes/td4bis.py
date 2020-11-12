66b48fa2234b4b691f6912bd14e5d5aeef802bd5
Kévin JOSEPH <kevin.joseph@ens.uvsq.fr>
2020-11-12 10:36:38 +0100

    1

--- exercises/TD04_listes/td4bis.py
+++ exercises/TD04_listes/td4bis.py
@@ -1,10 +1,15 @@
 def syracuse(n):
-    """ Retourne la liste des valeurs de la suite en partant de n jusqu'à 1 """
-    pass
-
+    l = []
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n *= 3 + 1
+        l.append(n)
+    return l
 print(syracuse(3))
 
-print(1 * 3 + 1)
-print(2 // 2 )
-print(3 * 3 + 1)
+#print(1 * 3 + 1)
+#print(2 // 2 )
+#print(3 * 3 + 1)
 
