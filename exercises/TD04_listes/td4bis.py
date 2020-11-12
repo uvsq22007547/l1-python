43159e4db018aa7f0779d592a0b69fa1399d4d0f
Kévin JOSEPH <kevin.joseph@ens.uvsq.fr>
2020-11-12 10:53:33 +0100

    1

--- exercises/TD04_listes/td4bis.py
+++ exercises/TD04_listes/td4bis.py
@@ -4,12 +4,15 @@ def syracuse(n):
         if n % 2 == 0:
             n = n // 2
         else:
-            n *= 3 + 1
+            n = n * 3 + 1
         l.append(n)
     return l
-print(syracuse(3))
+#print(syracuse(3))
 
-#print(1 * 3 + 1)
-#print(2 // 2 )
-#print(3 * 3 + 1)
+def testeConjecture(n_max):
+    for i in range(2, n_max+1):
+        syracuse(i)
+    return
+#print(testeConjecture(10000))
+
 
