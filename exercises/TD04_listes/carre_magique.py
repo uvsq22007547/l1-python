d5421a477a93dea07af6c05824baa64ac7da4108
Kévin JOSEPH <kevin.joseph@ens.uvsq.fr>
2020-11-19 10:51:43 +0100

    .

--- exercises/TD04_listes/carre_magique.py
+++ exercises/TD04_listes/carre_magique.py
@@ -14,5 +14,28 @@ carre_pas_mag = list(carre_mag)
 for i in range(len(carre_pas_mag)):
     carre_pas_mag[i] = list(carre_pas_mag[i])
 carre_pas_mag[3][2] = 7
-print(carre_pas_mag)
-print(carre_mag)
+# print(carre_pas_mag)
+# print(carre_mag)
+
+
+def afficheCarre(carre):
+    """ Affiche la liste à 2 dimensions carre comme un carré"""
+    for ligne in carre:
+        print(ligne)
+    print()
+
+# afficheCarre(carre_mag)
+# afficheCarre(carre_pas_mag)
+
+
+def testLignesEgales(carre):
+    """ Renvoie la somme des éléments d'une ligne de la liste 2D carre si toutes les lignes ont la même somme, et -1 sinon """
+    somme = sum(carre[0]) # somme des élements de la premières ligne
+    for ligne in carre:
+        if sum(ligne) != somme:
+            return -1
+    return somme
+    
+
+print(testLignesEgales(carre_mag))
+print(testLignesEgales(carre_pas_mag))
